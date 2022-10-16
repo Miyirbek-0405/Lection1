8c8e0083680849bbb12f690ec38829b14c794495
Miyirbek <miyirbekmaxsetuli@gmail.com>
2022-10-16 13:10:55 +0500

    change example 003

--- example003__sum/Program.cs
+++ example003__sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 7;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine($"номерА {numberA}");
+int numberB = new Random().Next(1,10);
+Console.WriteLine($"номерБ {numberB}");
 int result = numberA + numberB;
 Console.WriteLine(result);
