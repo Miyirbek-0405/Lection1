6c8c2d47d14517065efa43014cb173e417c08a31
Miyirbek <miyirbekmaxsetuli@gmail.com>
2022-10-16 14:33:52 +0500

    add example 005

---
+++ example005__HelloMasha/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша"){
+    Console.WriteLine("Ура, Это же Маша!");
+}
+else{
+    Console.WriteLine($"Привет, {username}");
+}
